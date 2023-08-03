bc7854325a117aa9e6339a645f9b4e319e8e567a
Rapha149 <raphael@raphael-jantzen.de>
2023-08-03 16:59:16 +0200

    Update javadoc.

--- api/src/main/java/de/rapha149/signgui/SignGUIResult.java
+++ api/src/main/java/de/rapha149/signgui/SignGUIResult.java
@@ -32,6 +32,7 @@ public class SignGUIResult {
     /**
      * Used for getting the lines of the sign without color codes.
      * Calls {@link org.bukkit.ChatColor#stripColor(String)} on each line.
+     * This method only strips color codes with § (not with & for example).
      *
      * @return The lines of the sign without color codes.
      */
@@ -45,6 +46,7 @@ public class SignGUIResult {
     /**
      * Used for getting a specific line of the sign without color codes.
      * Calls {@link org.bukkit.ChatColor#stripColor(String)} on the line.
+     * This method only strips color codes with § (not with & for example).
      *
      * @param index The index of the desired line.
      * @return The line at the given index without color codes.
